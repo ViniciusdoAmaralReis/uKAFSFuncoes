92da662295e1fdbae8069175e20e91a1a7c414ee
Vinicius do Amaral Reis <viniciusdoamaralreis@gmail.com>
2025-10-03 11:17:14 -0300

    Update uKAFSFuncoes.pas

--- uKAFSFuncoes.pas
+++ uKAFSFuncoes.pas
@@ -3,11 +3,11 @@ unit uKAFSFuncoes;
 interface
 
 uses
-  System.Classes, System.IniFiles, System.IOUtils, System.Net.HttpClient,
-  System.Net.HttpClientComponent, System.Net.URLClient, System.NetEncoding,
-  System.SysUtils, System.Types,
+  System.Classes, System.IniFiles, System.IOUtils, System.Math,
+  System.Net.HttpClient, System.Net.HttpClientComponent, System.Net.URLClient,
+  System.NetEncoding, System.SysUtils, System.Types,
   IdIPWatch, IdStack
-  {$IF DEFINED(FMX) OR DEFINED(SKIA)}
+  {$IFNDEF CONSOLE}
   , FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Platform
   {$ENDIF}
   {$IFDEF MSWINDOWS}
@@ -20,18 +20,23 @@ uses
   ;
 
   function NomeProjeto: String;
-  {$IF DEFINED(FMX) OR DEFINED(SKIA)}
+  {$IFNDEF CONSOLE}
   function ResolucaoNativa: TPoint;
   function ComponenteRectF(const _componente: TControl): TRectF;
+  function PontoCentral(_componente: TControl): TPointF;
+  function Colisao(_rect1, _rect2: TRectF): Boolean;
+  function AnguloRotacao(const _componente: TControl; const _alvoX, _alvoY: Single): Single;
+  function Distancia(const _componente1, _component2: TControl): Single;
   procedure AbrirNavegador(const _url: String);
   procedure Vibrar;
   function CacheParaBmp(const _nome: String): FMX.Graphics.TBitmap;
   function URLParaBmp(const _url: String): FMX.Graphics.TBitmap;
   function Base64ParaBmp(const _img: String): FMX.Graphics.TBitmap;
   {$ENDIF}
+  function VelocidadeParaDuracao(const _velociade: Single; const _inicioX, _inicioY, _fimX, _fimY: Single): Single;
   function BarraProgresso(const _valor, _total, _barra: Single): Single;
-  function Codificar(const _texto: String): String;
-  function Decodificar(const _texto: String): String;
+  function TextoParaBase64(const _texto: String): String;
+  function Base64ParaTexto(const _base64: String): String;
   procedure SalvarIni(const _arquivo: String; _secao, _campo, _valor: String);
   function LerIni(const _arquivo: String; _secao, _campo: String): String;
   function IPPrivado: String;
@@ -44,7 +49,7 @@ begin
   Result := TPath.GetFileNameWithoutExtension(ParamStr(0));
 end;
 
-{$IF DEFINED(FMX) OR DEFINED(SKIA)}
+{$IFNDEF CONSOLE}
 function ResolucaoNativa: TPoint;
 begin
   var ScreenService: IFMXScreenService;
@@ -64,6 +69,7 @@ begin
       Round(Screen.Size.Height));
   end;
 end;
+// Verificar, trocar TCOntrol por posições somente
 function ComponenteRectF(const _componente: TControl): TRectF;
 begin
   Result := RectF(_componente.Position.X,
@@ -71,12 +77,44 @@ begin
                   _componente.Position.X + _componente.Width,
                   _componente.Position.Y + _componente.Height);
 end;
+function PontoCentral(_componente: TControl): TPointF;
+begin
+  Result := TPointF.Create(_componente.Position.X + (_componente.Width / 2), _componente.Position.Y + (_componente.Height / 2));
+end;
+function Colisao(_rect1, _rect2: TRectF): Boolean;
+begin
+  Result := _rect1.IntersectsWith(_rect2);
+  //Result := _componente1.BoundsRect.IntersectsWith(_componente2.BoundsRect);
+end;
+function AnguloRotacao(const _componente: TControl; const _alvoX, _alvoY: Single): Single;
+begin
+  var _centrocomponente := PontoCentral(_componente);
+  var _angulo := ArcTan2(_alvoY - _centrocomponente.Y, _alvoX - _centrocomponente.X);
+
+  _angulo := _angulo * (180 / Pi) + 90; // +90 para frente apontar para destino
+
+  Result := _angulo;
+end;
+function Distancia(const _componente1, _component2: TControl): Single;
+begin
+  var CentroA := PontoCentral(_componente1);
+  var CentroB := PontoCentral(_component2);
+
+  var DeltaX := CentroB.X - CentroA.X;
+  var DeltaY := CentroB.Y - CentroA.Y;
+
+  Result := Sqrt(DeltaX * DeltaX + DeltaY * DeltaY);
+end;
 
 procedure AbrirNavegador(const _url: String);
 begin
   // Abre o navegador padrão do sistema
   {$IFDEF MSWINDOWS}
   ShellExecute(0, 'open', PChar(_url), nil, nil, SW_SHOWNORMAL);
+  {$ENDIF}
+
+  {$IFDEF LINUX}
+  _system(PAnsiChar('xdg-open ' + AnsiString(_url)));
   {$ENDIF}
 
   {$IFDEF ANDROID}
@@ -151,6 +189,13 @@ begin
 end;
 {$ENDIF}
 
+function VelocidadeParaDuracao(const _velociade: Single; const _inicioX, _inicioY, _fimX, _fimY: Single): Single;
+begin
+  var _distanciaX := Abs(_fimX - _inicioX);
+  var _distanciaY := Abs(_fimY - _inicioY);
+
+  Result := Sqrt(Sqr(_distanciaX) + Sqr(_distanciaY)) / _velociade;
+end;
 function BarraProgresso(const _valor, _total, _barra: Single): Single;
 begin
   // Calcula o valor do progresso
@@ -161,7 +206,7 @@ begin
     Result := (_valor / _total) * _barra;
 end;
 
-function Codificar(const _texto: String): String;
+function TextoParaBase64(const _texto: String): String;
 begin
   var Base64 := TBase64Encoding.Create(0, ''); // 0 = Sem quebra de linha
   try
@@ -174,24 +219,29 @@ begin
     FreeAndNil(Base64);
   end;
 end;
-function Decodificar(const _texto: String): String;
+function Base64ParaTexto(const _base64: String): String;
 begin
   // Substitui caracteres especiais para arquivos INI
   Result := TNetEncoding.Base64.Decode(
-    _texto.Replace('-', '+')
-          .Replace('!', '/')
-          .Replace('$', '='));
+    _base64.Replace('-', '+')
+           .Replace('!', '/')
+           .Replace('$', '='));
 end;
 
 procedure SalvarIni(const _arquivo: String; _secao, _campo, _valor: String);
 begin
   // Codifica as strings
-  _secao := Codificar(_secao);
-  _campo := Codificar(_campo);
-  _valor := Codificar(_valor);
+  _secao := TextoParaBase64(_secao);
+  _campo := TextoParaBase64(_campo);
+  _valor := TextoParaBase64(_valor);
 
   // Determina o caminho e cria uma pasta caso não exista
+  {$IF defined(MSWINDOWS) or defined(LINUX)}
   var _caminho := ExtractFilePath(ParamStr(0));
+  {$ENDIF}
+  {$IFDEF ANDROID}
+  var _caminho := TPath.GetHomePath;
+  {$ENDIF}
   if not TDirectory.Exists(_caminho) then
     ForceDirectories(_caminho);
 
@@ -208,11 +258,16 @@ begin
   Result := '';
 
   // Codifica as strings
-  _secao := Codificar(_secao);
-  _campo := Codificar(_campo);
+  _secao := Base64ParaTexto(_secao);
+  _campo := Base64ParaTexto(_campo);
 
   // Determina o caminho de documentos e sua pasta
+  {$IF defined(MSWINDOWS) or defined(LINUX)}
   var _caminho := ExtractFilePath(ParamStr(0));
+  {$ENDIF}
+  {$IFDEF ANDROID}
+  var _caminho := TPath.GetHomePath;
+  {$ENDIF}
 
   // Cria e lê o arquivo INI
   var _ini := TIniFile.Create(System.IOUtils.TPath.Combine(_caminho, _arquivo + '.ini'));
@@ -221,7 +276,7 @@ begin
 
     // Decodifica caso não seja vazio
     if _valor <> '' then
-      Result := Decodificar(_valor);
+      Result := Base64ParaTexto(_valor);
   finally
     FreeAndNil(_ini);
   end;
